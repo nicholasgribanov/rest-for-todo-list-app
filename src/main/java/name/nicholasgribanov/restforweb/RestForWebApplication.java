6cd0a7314bfe7b22f48194281845dd532bd0b086
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2020-02-05 07:43:47 +0300

    Initial commit

---
+++ src/main/java/name/nicholasgribanov/restforweb/RestForWebApplication.java
@@ -0,0 +1,13 @@
+package name.nicholasgribanov.restforweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestForWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestForWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/name/nicholasgribanov/restforweb/RestForWebApplicationTests.java
@@ -0,0 +1,13 @@
+package name.nicholasgribanov.restforweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestForWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
