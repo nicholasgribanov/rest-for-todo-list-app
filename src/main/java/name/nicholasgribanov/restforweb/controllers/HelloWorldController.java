201bd8a6004836d0d7308931a26c8125292288d4
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2020-02-07 15:58:38 +0300

    Added method with parameter

--- src/main/java/name/nicholasgribanov/restforweb/controllers/HelloWorldController.java
+++ src/main/java/name/nicholasgribanov/restforweb/controllers/HelloWorldController.java
@@ -3,6 +3,7 @@ package name.nicholasgribanov.restforweb.controllers;
 import name.nicholasgribanov.restforweb.model.HelloWorld;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("*")
@@ -13,4 +14,11 @@ public class HelloWorldController {
     public HelloWorld getHelloWorld() {
         return new HelloWorld("Hello world!!!!");
     }
+
+    @GetMapping("/hello/{name}")
+    public HelloWorld getHelloWorldParameter(@PathVariable String name) {
+        return new HelloWorld("Hello world, " + name);
+    }
 }
+
+
